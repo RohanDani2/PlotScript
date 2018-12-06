4fd6efab26dbd7ef5de8b3b0e9254da53ee6ed01
Rohan Dani <rohan.dani1@gmail.com>
2018-12-05 21:53:14 -0500

    milestone 4 commit

--- plotscript.cpp
+++ plotscript.cpp
@@ -12,64 +12,6 @@
 #include "semantic_error.hpp"
 #include "multiThread.hpp"
 
-volatile sig_atomic_t global_status_flag = 0;
-
-#if defined(_WIN64) || defined(_WIN32)
-#include <windows.h>
-
-// this function is called when a signal is sent to the process
-BOOL WINAPI interrupt_handler(DWORD fdwCtrlType) {
-
-	switch (fdwCtrlType) {
-	case CTRL_C_EVENT: // handle Cnrtl-C
-					   // if not reset since last call, exit
-		if (global_status_flag > 0) {
-			exit(EXIT_FAILURE);
-		}
-		++global_status_flag;
-		return TRUE;
-
-	default:
-		return FALSE;
-	}
-}
-
-// install the signal handler
-inline void install_handler() { SetConsoleCtrlHandler(interrupt_handler, TRUE); }
-// *****************************************************************************
-
-// *****************************************************************************
-// install a signal handler for Cntl-C on Unix/Posix
-// *****************************************************************************
-#elif defined(__APPLE__) || defined(__linux) || defined(__unix) ||             \
-    defined(__posix)
-#include <unistd.h>
-
-// this function is called when a signal is sent to the process
-void interrupt_handler(int signal_num) {
-
-	if (signal_num == SIGINT) { // handle Cnrtl-C
-								// if not reset since last call, exit
-		if (global_status_flag > 0) {
-			exit(EXIT_FAILURE);
-		}
-		++global_status_flag;
-	}
-}
-
-// install the signal handler
-inline void install_handler() {
-
-	struct sigaction sigIntHandler;
-
-	sigIntHandler.sa_handler = interrupt_handler;
-	sigemptyset(&sigIntHandler.sa_mask);
-	sigIntHandler.sa_flags = 0;
-
-	sigaction(SIGINT, &sigIntHandler, NULL);
-}
-#endif
-
 struct queueStruct {
 	bool error;
 	std::string errorString;
